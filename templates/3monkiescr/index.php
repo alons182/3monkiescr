530fd7cc9e142da5a776a076aa30dc794b738007
alons182 <alons182@hotmail.com>
2013-09-16 19:02:52 -0600

    logo animado

--- templates/3monkiescr/index.php
+++ templates/3monkiescr/index.php
@@ -91,7 +91,45 @@ $doc->addStyleSheet('templates/'.$this->template.'/css/main.css');
                 </section>
                 <section id="right-container">
                     <div id="logo">
-                        <a href="<?php echo $this->baseurl ?>"><div id="contenedor_legenda"></div><div id="legenda"></div><img src="<?php echo $this->baseurl ?>/templates/3monkiescr/img/logo.png" alt="3 Mokies Logo"></a>
+                    <a href="<?php echo $this->baseurl ?>"><div id="contenedor_legenda"></div><div id="legenda"></div>
+
+                        
+                        <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="210" height="130" id="Logo animado 3Monkies" align="middle">
+                        <param name="movie" value="<?php echo $this->baseurl ?>/templates/3monkiescr/img/logo.swf" />
+                        <param name="quality" value="high" />
+                        <param name="bgcolor" value="#ffffff" />
+                        <param name="play" value="true" />
+                        <param name="loop" value="true" />
+                        <param name="wmode" value="transparent" />
+                        <param name="scale" value="showall" />
+                        <param name="menu" value="true" />
+                        <param name="devicefont" value="false" />
+                        <param name="salign" value="" />
+                        <param name="allowScriptAccess" value="sameDomain" />
+                        <!--[if !IE]>-->
+                        <object type="application/x-shockwave-flash" data="<?php echo $this->baseurl ?>/templates/3monkiescr/img/logo.swf" width="210" height="130">
+                            <param name="movie" value="<?php echo $this->baseurl ?>/templates/3monkiescr/img/logo.swf" />
+                            <param name="quality" value="high" />
+                            <param name="bgcolor" value="#ffffff" />
+                            <param name="play" value="true" />
+                            <param name="loop" value="true" />
+                            <param name="wmode" value="transparent" />
+                            <param name="scale" value="showall" />
+                            <param name="menu" value="true" />
+                            <param name="devicefont" value="false" />
+                            <param name="salign" value="" />
+                            <param name="allowScriptAccess" value="sameDomain" />
+                        <!--<![endif]-->
+                            <a href="http://www.adobe.com/go/getflash">
+                                <img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Get Adobe Flash player" />
+                            </a>
+                        <!--[if !IE]>-->
+                        </object>
+                        <!--<![endif]-->
+                    </object>
+                    </a>
+                        
+                        <!--<a href="<?php echo $this->baseurl ?>"><div id="contenedor_legenda"></div><div id="legenda"></div><img src="<?php echo $this->baseurl ?>/templates/3monkiescr/img/logo.png" alt="3 Mokies Logo"></a>-->
                     </div>
                     <nav id="menu">
                         <jdoc:include type="modules" name="menu" style="none" />
