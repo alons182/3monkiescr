c98c7dae93bb684f61ee1e4513fafcd87521b859
alons182 <alons182@hotmail.com>
2013-09-19 17:09:11 -0600

    error 404 costum

--- templates/3monkiescr/error.php
+++ templates/3monkiescr/error.php
@@ -9,50 +9,13 @@
 
 defined('_JEXEC') or die;
 
-// Getting params from template
-$params = JFactory::getApplication()->getTemplate(true)->params;
-
 $app = JFactory::getApplication();
 $doc = JFactory::getDocument();
 $this->language = $doc->language;
-$this->direction = $doc->direction;
 
-// Detecting Active Variables
-$option   = $app->input->getCmd('option', '');
-$view     = $app->input->getCmd('view', '');
-$layout   = $app->input->getCmd('layout', '');
-$task     = $app->input->getCmd('task', '');
+
 $itemid   = $app->input->getCmd('Itemid', '');
-$sitename = $app->getCfg('sitename');
 
-if($task == "edit" || $layout == "form" )
-{
-	$fullWidth = 1;
-}
-else
-{
-	$fullWidth = 0;
-}
-
-// Add JavaScript Frameworks
-JHtml::_('bootstrap.framework');
-
-// Add current user information
-$user = JFactory::getUser();
-
-
-// Logo file
-if ($params->get('logoFile'))
-{
-	$logo = JURI::root() . $params->get('logoFile');
-}
-else
-{
-	$logo = $this->baseurl . "/templates/" . $this->template . "/images/logo.png";
-}
-
-$doc->addStyleSheet('templates/'.$this->template.'/css/normalize.min.css');
-$doc->addStyleSheet('templates/'.$this->template.'/css/main.css');
 ?>
 <!DOCTYPE html>
 <html lang="<?php echo $this->language; ?>" >
@@ -62,165 +25,175 @@ $doc->addStyleSheet('templates/'.$this->template.'/css/main.css');
 	<link rel="icon" type="image/png" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/img/favicon_32x32.ico">
 	 <link href="http://fonts.googleapis.com/css?family=Lato:100,300,700" rel="stylesheet" type="text/css">
      <link href="http://fonts.googleapis.com/css?family=Pontano+Sans" rel="stylesheet" type="text/css">
+     <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/normalize.min.css" type="text/css" />
+     <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/main.css" type="text/css" />
 
-	<?php
-		$debug = JFactory::getConfig()->get('debug_lang');
-		if ((defined('JDEBUG') && JDEBUG) || $debug)
-		{
-	?>
-		<link rel="stylesheet" href="<?php echo $this->baseurl ?>/media/cms/css/debug.css" type="text/css" />
-	<?php
-		}
-	?>
-	<?php
-	// If Right-to-Left
-	if ($this->direction == 'rtl')
-	{
-	?>
-		<link rel="stylesheet" href="<?php echo $this->baseurl ?>/media/jui/css/bootstrap-rtl.css" type="text/css" />
-	<?php
-	}
-	// Use of Google Font
-	if ($params->get('googleFont'))
-	{
-	?>
-		<link href='http://fonts.googleapis.com/css?family=<?php echo $params->get('googleFontName');?>' rel='stylesheet' type='text/css'>
-	<?php
-	}
-	?>
-	<link href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon" />
-	<?php
-	// Template color
-	if ($params->get('templateColor'))
-	{
-	?>
-	<style type="text/css">
-		body.site
-		{
-			border-top: 3px solid <?php echo $params->get('templateColor');?>;
-			background-color: <?php echo $params->get('templateBackgroundColor');?>
-		}
-		a
-		{
-			color: <?php echo $params->get('templateColor');?>;
-		}
-		.navbar-inner, .nav-list > .active > a, .nav-list > .active > a:hover, .dropdown-menu li > a:hover, .dropdown-menu .active > a, .dropdown-menu .active > a:hover, .nav-pills > .active > a, .nav-pills > .active > a:hover
-		{
-			background: <?php echo $params->get('templateColor');?>;
-		}
-		.navbar-inner
-		{
-			-moz-box-shadow: 0 1px 3px rgba(0, 0, 0, .25), inset 0 -1px 0 rgba(0, 0, 0, .1), inset 0 30px 10px rgba(0, 0, 0, .2);
-			-webkit-box-shadow: 0 1px 3px rgba(0, 0, 0, .25), inset 0 -1px 0 rgba(0, 0, 0, .1), inset 0 30px 10px rgba(0, 0, 0, .2);
-			box-shadow: 0 1px 3px rgba(0, 0, 0, .25), inset 0 -1px 0 rgba(0, 0, 0, .1), inset 0 30px 10px rgba(0, 0, 0, .2);
-		}
-	</style>
-	<?php
-	}
-	?>
-	<!--[if lt IE 9]>
-		<script src="<?php echo $this->baseurl ?>/media/jui/js/html5.js"></script>
-	<![endif]-->
+	
 </head>
+<body class="<?php echo ($itemid ? ' bgid-' . $itemid : '')?>">
+	
+	<div id="wrap">
+          
+            
+            <section id="main">
+                <section id="left-container">
 
-<body class="site <?php echo $option
-	. ' view-' . $view
-	. ($layout ? ' layout-' . $layout : ' no-layout')
-	. ($task ? ' task-' . $task : ' no-task')
-	. ($itemid ? ' itemid-' . $itemid : '')
-	. ($params->get('fluidContainer') ? ' fluid' : '');
-?>">
-
-	<!-- Body -->
-	<div class="body">
-		<div class="container<?php echo ($params->get('fluidContainer') ? '-fluid' : '');?>">
-			<!-- Header -->
-			<div class="header">
-				<div class="header-inner clearfix">
-					<a class="brand pull-left" href="<?php echo $this->baseurl; ?>">
-						<img src="<?php echo $logo;?>" alt="<?php echo $sitename; ?>" />
-					</a>
-					<div class="header-search pull-right">
-						<?php
-						// Display position-0 modules
-						$this->searchmodules = JModuleHelper::getModules('position-0');
-						foreach ($this->searchmodules as $searchmodule)
-						{
-							$output = JModuleHelper::renderModule($searchmodule, array('style' => 'none'));
-							$params = new JRegistry;
-							$params->loadString($searchmodule->params);
-							echo $output;
-						}
-						?>
-					</div>
-				</div>
-			</div>
-			<div class="navigation">
-				<?php
-				// Display position-1 modules
-				$this->navmodules = JModuleHelper::getModules('position-1');
-				foreach ($this->navmodules as $navmodule)
-				{
-					$output = JModuleHelper::renderModule($navmodule, array('style' => 'none'));
-					$params = new JRegistry;
-					$params->loadString($navmodule->params);
-					echo $output;
-				}
-				?>
-			</div>
-			<!-- Banner -->
-			<div class="banner">
-				<jdoc:include type="modules" name="banner" style="xhtml" />
-			</div>
-			<div class="row-fluid">
-				<div id="content" class="span12">
-					<!-- Begin Content -->
-					<h1 class="page-header"><?php echo JText::_('JERROR_LAYOUT_PAGE_NOT_FOUND'); ?>sssssss</h1>
-					<div class="well">
-						<div class="row-fluid">
-							<div class="span6">
-								<p><strong><?php echo JText::_('JERROR_LAYOUT_ERROR_HAS_OCCURRED_WHILE_PROCESSING_YOUR_REQUEST'); ?></strong></p>
-								<p><?php echo JText::_('JERROR_LAYOUT_NOT_ABLE_TO_VISIT'); ?></p>
-								<ul>
-									<li><?php echo JText::_('JERROR_LAYOUT_AN_OUT_OF_DATE_BOOKMARK_FAVOURITE'); ?></li>
-									<li><?php echo JText::_('JERROR_LAYOUT_MIS_TYPED_ADDRESS'); ?></li>
-									<li><?php echo JText::_('JERROR_LAYOUT_SEARCH_ENGINE_OUT_OF_DATE_LISTING'); ?></li>
-									<li><?php echo JText::_('JERROR_LAYOUT_YOU_HAVE_NO_ACCESS_TO_THIS_PAGE'); ?></li>
-								</ul>
+                    <div id="menu-top">
+                      
+                        <?php if (JModuleHelper::getModule('menu-top')) : ?>
+                        <?php
+										$mt = JModuleHelper::getModule('menu-top');
+										echo JModuleHelper::renderModule($mt);
+									?>
+						<?php endif; ?>
+                    </div>
+                    <div id="reservationbox">
+                        <div id="dialog" class="window">
+                        	<?php if (JModuleHelper::getModule('reservation-box')) : ?>
+                        <?php
+										$rb = JModuleHelper::getModule('reservation-box');
+										echo JModuleHelper::renderModule($rb);
+									?>
+						<?php endif; ?>
+                            <jdoc:include type="modules" name="reservation-box" style="none" />
+                        </div>
+                    </div>
+                    <div id="callbox">
+                        <div id="dialogC" class="window">
+                            <jdoc:include type="modules" name="call-box" style="none" />
+                        </div>
+                    </div>
+                    <div id="contactbox">
+                        <div id="dialogT" class="window">
+                            <jdoc:include type="modules" name="contact-box" style="none" />
+                        </div>
+                    </div>
+                    <div id="contenido">
+                       <div class="text404">
+                       		<h1 class="page-header"><?php echo JText::_('JERROR_LAYOUT_PAGE_NOT_FOUND'); ?></h1>
+								<div class="well">
+								<div class="row-fluid">
+									<div class="span6">
+										<p><strong><?php echo JText::_('JERROR_LAYOUT_ERROR_HAS_OCCURRED_WHILE_PROCESSING_YOUR_REQUEST'); ?></strong></p>
+										<p><?php echo JText::_('JERROR_LAYOUT_NOT_ABLE_TO_VISIT'); ?></p>
+										<ul>
+											<li><?php echo JText::_('JERROR_LAYOUT_AN_OUT_OF_DATE_BOOKMARK_FAVOURITE'); ?></li>
+											<li><?php echo JText::_('JERROR_LAYOUT_MIS_TYPED_ADDRESS'); ?></li>
+											<li><?php echo JText::_('JERROR_LAYOUT_SEARCH_ENGINE_OUT_OF_DATE_LISTING'); ?></li>
+											<li><?php echo JText::_('JERROR_LAYOUT_YOU_HAVE_NO_ACCESS_TO_THIS_PAGE'); ?></li>
+										</ul>
+									</div>
+									<div class="span6">
+										<?php if (JModuleHelper::getModule('search')) : ?>
+											<p><strong><?php echo JText::_('JERROR_LAYOUT_SEARCH'); ?></strong></p>
+											<p><?php echo JText::_('JERROR_LAYOUT_SEARCH_PAGE'); ?></p>
+											<?php
+												$module = JModuleHelper::getModule('search');
+												echo JModuleHelper::renderModule($module);
+											?>
+										<?php endif; ?>
+										<p><?php echo JText::_('JERROR_LAYOUT_GO_TO_THE_HOME_PAGE'); ?></p>
+										<p><a href="<?php echo $this->baseurl; ?>/index.php" class="btn"><i class="icon-home"></i> <?php echo JText::_('JERROR_LAYOUT_HOME_PAGE'); ?></a></p>
+									</div>
+								</div>
+								<hr />
+								<p><?php echo JText::_('JERROR_LAYOUT_PLEASE_CONTACT_THE_SYSTEM_ADMINISTRATOR'); ?></p>
+								<blockquote>
+									<span class="label label-inverse"><?php echo $this->error->getCode(); ?></span> <?php echo $this->error->getMessage();?>
+								</blockquote>
 							</div>
-							<div class="span6">
-								<?php if (JModuleHelper::getModule('search')) : ?>
-									<p><strong><?php echo JText::_('JERROR_LAYOUT_SEARCH'); ?></strong></p>
-									<p><?php echo JText::_('JERROR_LAYOUT_SEARCH_PAGE'); ?></p>
-									<?php
-										$module = JModuleHelper::getModule('search');
+
+                       </div>
+                        
+                    </div>
+                </section>
+                <section id="right-container">
+                    <div id="logo">
+                    <a href="<?php echo $this->baseurl ?>"><div id="contenedor_legenda"></div><div id="legenda"></div>
+
+                     <div id="mascara"></div>
+                     <div id="logo_animado">
+                         <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="210" height="130" id="Logo animado 3Monkies" align="middle">
+                        <param name="movie" value="<?php echo $this->baseurl ?>/templates/3monkiescr/img/logo.swf" />
+                        <param name="quality" value="high" />
+                        <param name="bgcolor" value="#ffffff" />
+                        <param name="play" value="true" />
+                        <param name="loop" value="true" />
+                        <param name="wmode" value="transparent" />
+                        <param name="scale" value="showall" />
+                        <param name="menu" value="true" />
+                        <param name="devicefont" value="false" />
+                        <param name="salign" value="" />
+                        <param name="allowScriptAccess" value="sameDomain" />
+                        <!--[if !IE]>-->
+                        <object type="application/x-shockwave-flash" data="<?php echo $this->baseurl ?>/templates/3monkiescr/img/logo.swf" width="210" height="130">
+                            <param name="movie" value="<?php echo $this->baseurl ?>/templates/3monkiescr/img/logo.swf" />
+                            <param name="quality" value="high" />
+                            <param name="bgcolor" value="#ffffff" />
+                            <param name="play" value="true" />
+                            <param name="loop" value="true" />
+                            <param name="wmode" value="transparent" />
+                            <param name="scale" value="showall" />
+                            <param name="menu" value="true" />
+                            <param name="devicefont" value="false" />
+                            <param name="salign" value="" />
+                            <param name="allowScriptAccess" value="sameDomain" />
+                        <!--<![endif]-->
+                            <a href="http://www.adobe.com/go/getflash">
+                                <img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Get Adobe Flash player" />
+                            </a>
+                        <!--[if !IE]>-->
+                        </object>
+                        <!--<![endif]-->
+                    </object>
+                     </div>   
+                        
+                    </a>
+                        
+                        
+                    </div>
+                    <nav id="menu">
+                        <?php if (JModuleHelper::getModule('menu')) : ?>
+                        <?php
+										$module = JModuleHelper::getModule('menu');
 										echo JModuleHelper::renderModule($module);
 									?>
-								<?php endif; ?>
-								<p><?php echo JText::_('JERROR_LAYOUT_GO_TO_THE_HOME_PAGE'); ?></p>
-								<p><a href="<?php echo $this->baseurl; ?>/index.php" class="btn"><i class="icon-home"></i> <?php echo JText::_('JERROR_LAYOUT_HOME_PAGE'); ?></a></p>
-							</div>
-						</div>
-						<hr />
-						<p><?php echo JText::_('JERROR_LAYOUT_PLEASE_CONTACT_THE_SYSTEM_ADMINISTRATOR'); ?></p>
-						<blockquote>
-							<span class="label label-inverse"><?php echo $this->error->getCode(); ?></span> <?php echo $this->error->getMessage();?>
-						</blockquote>
-					</div>
-					<!-- End Content -->
-				</div>
-			</div>
-		</div>
-	</div>
-	<!-- Footer -->
-	<div class="footer">
-		<div class="container<?php echo ($params->get('fluidContainer') ? '-fluid' : '');?>">
-			<hr />
-			<jdoc:include type="modules" name="footer" style="none" />
-			<p class="pull-right"><a href="#top" id="back-top"><?php echo JText::_('TPL_PROTOSTAR_BACKTOTOP'); ?></a></p>
-			<p>&copy; <?php echo $sitename; ?> <?php echo date('Y');?></p>
-		</div>
-	</div>
+						<?php endif; ?>
+                    </nav>
+                </section>
+            </section>
+        </div>
+        <footer>
+            <div id="bottom">
+                <nav id="menu-bottom">
+                    <jdoc:include type="modules" name="menu-bottom" style="none" />
+                </nav>
+                <div id="redes">
+                    <jdoc:include type="modules" name="redes" style="none" />
+                </div>
+                <div id="patners">
+                    <jdoc:include type="modules" name="patners" style="none" />
+                </div>
+                <div id="copyright">
+                    <a href="http://www.avotz.com" target="_blank">Copyright &copy; 2013 | Avotz</a >
+                </div>
+            </div>
+        </footer>
+
+      
+        
+        <script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/js/vendor/jquery-1.10.1.min.js"></script>
+
+        <script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/js/main.js"></script>
+
+        <script>
+            /*var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src='//www.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g,s)}(document,'script'));*/
+        </script>
+
 	<jdoc:include type="modules" name="debug" style="none" />
 </body>
+
 </html>
